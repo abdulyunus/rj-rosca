0a454cb612829c4bf52ef6c2eb3037c2683afc64
Abdul Yunus <abdul.yg@gmail.com>
2026-04-09 01:17:03 +0530

    Update gsheet_client.py

--- gsheet_client.py
+++ gsheet_client.py
@@ -1,15 +1,16 @@
-import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import streamlit as st
+import json
 
 def get_gspread_client():
     scope = [
         'https://www.googleapis.com/auth/spreadsheets',
         'https://www.googleapis.com/auth/drive'
     ]
-
-    creds_dict = st.secrets["gcp_service_account"]
-
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-
+    # Write credentials from Streamlit secrets to a file if running on Streamlit Cloud
+    if "google" in st.secrets and "gcp_service_account" in st.secrets["google"]:
+        with open("credentials.json", "w") as f:
+            f.write(st.secrets["google"]["gcp_service_account"])
+    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
     return gspread.authorize(creds)
