0902cc0c13627be2a0baba28f9e91b754d71b456
Abdul Yunus <abdul.yg@gmail.com>
2026-04-09 00:36:05 +0530

    Refactor gspread client to use Streamlit secrets
    Updated the get_gspread_client function to use Streamlit secrets for credentials.

--- gsheet_client.py
+++ gsheet_client.py
@@ -1,4 +1,4 @@
-import gspread
+import streamlit as st
 from oauth2client.service_account import ServiceAccountCredentials
 
 def get_gspread_client():
@@ -6,5 +6,9 @@ def get_gspread_client():
         'https://www.googleapis.com/auth/spreadsheets',
         'https://www.googleapis.com/auth/drive'
     ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+
+    creds_dict = st.secrets["gcp_service_account"]
+
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
     return gspread.authorize(creds)
